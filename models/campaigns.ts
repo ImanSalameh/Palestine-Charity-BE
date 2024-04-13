e626475210a59f0b502be76bb9ba9c981e64cb08
Nagham202 <naghamatawel@gmail.com>
2024-04-13 04:34:48 +0300

    status fixed

--- models/campaigns.ts
+++ models/campaigns.ts
@@ -1,11 +1,12 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
+// Define interface for Campaign document
 export interface ICampaign extends Document {
     campaignName: string;
     campaignImage: string;
     organizationName: string;
     goalAmount: number;
-    status: string;
+    status: "Active" | "Suspended" | "Ended"; // Union type for allowed values
     currentAmount?: number; // Making currentAmount optional
     startDate: Date;
     endDate: Date;
@@ -13,17 +14,19 @@ export interface ICampaign extends Document {
     description?: string;
 }
 
+// Define schema for Campaign
 const campaignSchema: Schema<ICampaign> = new Schema<ICampaign>({
     campaignName: { type: String, required: true },
     campaignImage: { type: String, required: true },
     organizationName: { type: String, required: true },
     goalAmount: { type: Number, required: true },
-    status: { type: String, required: true },
+    status: { type: String, enum: ["Active", "Suspended", "Ended"], required: true }, // Specify allowed values
     currentAmount: { type: Number }, // Optional currentAmount
     startDate: { type: Date, required: true },
     endDate: { type: Date, required: true },
     leaderboard: { type: [String], default: [] },
     description: { type: String }
 });
+
 
 export const Campaign = mongoose.model<ICampaign>('Campaign', campaignSchema);
